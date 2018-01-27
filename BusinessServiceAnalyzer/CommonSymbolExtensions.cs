e120fc6381ff7ab08a78a8a259ba0550336aa408
IInspectable <github@smjh.de>
2018-01-27 12:11:03 +0100

    Cleanup

--- BusinessServiceAnalyzer/CommonSymbolExtensions.cs
+++ BusinessServiceAnalyzer/CommonSymbolExtensions.cs
@@ -23,24 +23,7 @@ namespace Pharmatechnik.Apotheke.XTplus.Analyzer {
         public static bool IsDeclaredPublic(this ISymbol symbol) {
             return symbol.DeclaredAccessibility == Accessibility.Public;
         }
-
-        public static string GetFullNamespace(this ISymbol symbol) {
-            if (string.IsNullOrEmpty(symbol.ContainingNamespace?.Name)) {
-                return null;
-            }
-
-            // get the rest of the full namespace string
-            string restOfResult = symbol.ContainingNamespace.GetFullNamespace();
-
-            string result = symbol.ContainingNamespace.Name;
-
-            if (restOfResult != null)
-                // if restOfResult is not null, append it after a period
-                result = restOfResult + '.' + result;
-
-            return result;
-        }
-
+       
         public static string ToMinimallyQualifiedDisplayString(this IMethodSymbol symbol) {
             return symbol.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat);
         }
@@ -48,32 +31,7 @@ namespace Pharmatechnik.Apotheke.XTplus.Analyzer {
         public static string ToFullyQualifiedFormatDisplayString(this INamedTypeSymbol symbol) {
             return symbol.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
         }
-
-        public static string GetFullTypeString(this INamedTypeSymbol type) {
-            string result = type.Name;
-
-            if (!type.TypeArguments.Any()) {
-                return result;
-            }
-
-            result += "<";
-
-            bool isFirstIteration = true;
-            foreach (var typeArg in type.TypeArguments.OfType<INamedTypeSymbol>()) {
-                if (isFirstIteration) {
-                    isFirstIteration = false;
-                } else {
-                    result += ", ";
-                }
-
-                result += typeArg.GetFullTypeString();
-            }
-
-            result += ">";
-
-            return result;
-        }
-
+        
         public static ImmutableArray<INamedTypeSymbol> AllBasesTypesAndSelf(this INamedTypeSymbol classSymbol) {
 
             if (classSymbol.TypeKind != TypeKind.Class) {
